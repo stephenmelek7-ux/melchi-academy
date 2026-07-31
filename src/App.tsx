bb7d925d15390abd62d12c9ec224b4170ebfa2b2
stephenmelek7-ux <stephenmelek7@gmail.com>
2026-07-31 12:40:49 -0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,9 @@ import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { Toaster } from 'react-hot-toast'
 
-// Public Pages
 import Home from './pages/public/Home'
 import About from './pages/public/About'
 import Contact from './pages/public/Contact'
-
-// Auth Pages
 import Login from './pages/auth/Login'
 import Register from './pages/auth/Register'
 
@@ -16,25 +13,13 @@ function App() {
     <Router>
       <div className="min-h-screen bg-gray-50">
         <Routes>
-          {/* Public Routes */}
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/contact" element={<Contact />} />
-          
-          {/* Auth Routes */}
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
         </Routes>
-        <Toaster 
-          position="top-right"
-          toastOptions={{
-            duration: 4000,
-            style: {
-              background: '#363636',
-              color: '#fff',
-            },
-          }}
-        />
+        <Toaster position="top-right" />
       </div>
     </Router>
   )
