2d474d58d3e10463bb863947e32ee5c288f5b609
stephenmelek7-ux <stephenmelek7@gmail.com>
2026-08-01 08:38:46 -0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { Toaster } from 'react-hot-toast'
+import { AuthProvider } from './context/AuthContext'
+import PrivateRoute from './routes/PrivateRoute'
 
 // Public Pages
 import Home from './pages/public/Home'
@@ -40,49 +42,55 @@ import AdminReports from './pages/admin/AdminReports'
 
 function App() {
   return (
-    <Router>
-      <div className="min-h-screen bg-gray-50">
-        <Routes>
-          {/* Public Routes */}
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/vision-mission" element={<VisionMission />} />
-          <Route path="/core-values" element={<CoreValues />} />
-          <Route path="/history" element={<History />} />
-          <Route path="/proprietor-welcome" element={<ProprietorWelcome />} />
-          <Route path="/principal-welcome" element={<PrincipalWelcome />} />
-          <Route path="/academic-programmes" element={<AcademicProgrammes />} />
-          <Route path="/admissions" element={<Admissions />} />
-          <Route path="/gallery" element={<Gallery />} />
-          <Route path="/news-events" element={<NewsEvents />} />
-          <Route path="/calendar" element={<Calendar />} />
-          <Route path="/faq" element={<FAQ />} />
-          <Route path="/prospectus" element={<Prospectus />} />
+    <AuthProvider>
+      <Router>
+        <div className="min-h-screen bg-gray-50">
+          <Routes>
+            {/* Public Routes */}
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/vision-mission" element={<VisionMission />} />
+            <Route path="/core-values" element={<CoreValues />} />
+            <Route path="/history" element={<History />} />
+            <Route path="/proprietor-welcome" element={<ProprietorWelcome />} />
+            <Route path="/principal-welcome" element={<PrincipalWelcome />} />
+            <Route path="/academic-programmes" element={<AcademicProgrammes />} />
+            <Route path="/admissions" element={<Admissions />} />
+            <Route path="/gallery" element={<Gallery />} />
+            <Route path="/news-events" element={<NewsEvents />} />
+            <Route path="/calendar" element={<Calendar />} />
+            <Route path="/faq" element={<FAQ />} />
+            <Route path="/prospectus" element={<Prospectus />} />
 
-          {/* Auth Routes */}
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/forgot-password" element={<ForgotPassword />} />
+            {/* Auth Routes */}
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/forgot-password" element={<ForgotPassword />} />
 
-          {/* Admin Routes */}
-          <Route path="/admin" element={<AdminLayout />}>
-            <Route index element={<AdminDashboard />} />
-            <Route path="content" element={<AdminContent />} />
-            <Route path="users" element={<AdminUsers />} />
-            <Route path="approvals" element={<AdminApprovals />} />
-            <Route path="students" element={<AdminStudents />} />
-            <Route path="teachers" element={<AdminTeachers />} />
-            <Route path="classes" element={<AdminClasses />} />
-            <Route path="subjects" element={<AdminSubjects />} />
-            <Route path="fees" element={<AdminFees />} />
-            <Route path="results" element={<AdminResults />} />
-            <Route path="reports" element={<AdminReports />} />
-          </Route>
-        </Routes>
-        <Toaster position="top-right" />
-      </div>
-    </Router>
+            {/* Admin Routes - Protected */}
+            <Route path="/admin" element={
+              <PrivateRoute requiredRole="admin">
+                <AdminLayout />
+              </PrivateRoute>
+            }>
+              <Route index element={<AdminDashboard />} />
+              <Route path="content" element={<AdminContent />} />
+              <Route path="users" element={<AdminUsers />} />
+              <Route path="approvals" element={<AdminApprovals />} />
+              <Route path="students" element={<AdminStudents />} />
+              <Route path="teachers" element={<AdminTeachers />} />
+              <Route path="classes" element={<AdminClasses />} />
+              <Route path="subjects" element={<AdminSubjects />} />
+              <Route path="fees" element={<AdminFees />} />
+              <Route path="results" element={<AdminResults />} />
+              <Route path="reports" element={<AdminReports />} />
+            </Route>
+          </Routes>
+          <Toaster position="top-right" />
+        </div>
+      </Router>
+    </AuthProvider>
   )
 }
 
